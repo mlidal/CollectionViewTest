c0b977ac54f3974e290013a0b7334d3b147a36a5
Mathias Mølster Lidal <mathiaslidal@gmail.com>
2017-01-09 10:56:48 +0100

    Initial commit

---
+++ ColllectionViewTest/ViewController.swift
@@ -0,0 +1,46 @@
+//
+//  ViewController.swift
+//  ColllectionViewTest
+//
+//  Created by Mathias Lidal on 09/01/2017.
+//  Copyright © 2017 Bouvet ASA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 4
+    }
+
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 4
+    }
+    
+    
+    func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+        let header = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: "SectionHeader", for: indexPath)
+        let label = header.viewWithTag(1) as! UILabel
+        label.text = "Header \(indexPath.section)"
+        return header
+    }
+
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CollectionCell", for: indexPath)
+        cell.backgroundColor = UIColor.blue
+        return cell
+    }
+}
+
